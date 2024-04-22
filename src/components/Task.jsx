999524d2d0ea091d34beb13bd7c29b4c5cfe14e8
bhuwan-darai <dr.darai29@gmail.com>
2024-04-22 14:27:09 +0545

    still ongoing font change

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,30 @@
-// import { FaEdit, FaRegTrashAlt } from "react-icons/fa";
-import { FaCheckDouble, FaPencil, FaTrashCan } from "react-icons/fa6";
+// // import { FaEdit, FaRegTrashAlt } from "react-icons/fa";
+// import { FaCheckDouble, FaPencil, FaTrashCan } from "react-icons/fa6";
+
+// const Task = ({ task, index, deleteTask, getSingleTask, setToComplete }) => {
+//   return (
+//     <div className={task.completed ? "task completed" : "task"}>
+//       <p>
+//         <b>{index + 1}</b>
+//         {task.name}
+//       </p>
+//       <div className="task-icons">
+//         <FaCheckDouble color="purple" onClick={() => setToComplete(task)} />
+//         <FaPencil color="green" onClick={() => getSingleTask(task)} />
+//         <FaTrashCan color="red" onClick={() => deleteTask(task._id)} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Task;
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckDouble,
+  faPencilAlt,
+  faTrashAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Task = ({ task, index, deleteTask, getSingleTask, setToComplete }) => {
   return (
@@ -9,9 +34,21 @@ const Task = ({ task, index, deleteTask, getSingleTask, setToComplete }) => {
         {task.name}
       </p>
       <div className="task-icons">
-        <FaCheckDouble color="purple" onClick={() => setToComplete(task)} />
-        <FaPencil color="green" onClick={() => getSingleTask(task)} />
-        <FaTrashCan color="red" onClick={() => deleteTask(task._id)} />
+        <FontAwesomeIcon
+          icon={faCheckDouble}
+          color="purple"
+          onClick={() => setToComplete(task)}
+        />
+        <FontAwesomeIcon
+          icon={faPencilAlt}
+          color="green"
+          onClick={() => getSingleTask(task)}
+        />
+        <FontAwesomeIcon
+          icon={faTrashAlt}
+          color="red"
+          onClick={() => deleteTask(task._id)}
+        />
       </div>
     </div>
   );
